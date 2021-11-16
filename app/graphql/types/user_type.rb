8b540221bc5bb320f716657f04789d1f4810bf12
kotaro1574 <shake1574@gmail.com>
2021-11-16 20:58:23 +0900

    GraphQLのオブジェクトの作成

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,15 @@
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: true
+    field :age, Integer, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :books, [Types::BookType], null: false
+    field :books_object_count, Integer, null: false
+     # ユーザーに紐づけられたBookオブジェクトの数を数える
+    def books_object_count
+          object.books.count
+        end
+  end
+end
